f0e26ae67d44bbb6be9f5f65cf86635d54026200
elizianeandreia <liziandreia@gmail.com>
2026-03-08 10:29:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ form.addEventListener('submit', function (e) {
 function verificarEmprestimo(tipo, renda, score) {
     var requisitos = {
         duplex: { renda: 60000, score: 700 },
-        condominio: { renda: 45000, score: 680 },
-        carro: { renda: 30000, score: 650 }
+        condominio: { renda: 4500, score: 680 },
+        carro: { renda: 3000, score: 650 }
     };
     var req = requisitos[tipo];
     if (!req) {
